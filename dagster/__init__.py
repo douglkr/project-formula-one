4934ca2f379e9d67e728b1b6e506db4b7c039f7e
Doug <152557999+douglkr@users.noreply.github.com>
2024-04-06 13:14:53 -0300

    refactor

--- dagster/__init__.py
+++ dagster/__init__.py
@@ -1,10 +1,10 @@
 from dagster import Definitions
 
-from analytics.resources import databricks_client_instance
-from analytics.assets.airbyte.airbyte import airbyte_ingestion
-from analytics.assets.databricks.databricks import databricks_workflow
-from analytics.jobs import formula_one_pipeline_asset_job
-from analytics.schedules import formula_one_pipeline_schedule
+from dagster.resources import databricks_client_instance
+from dagster.assets.airbyte.airbyte import airbyte_ingestion
+from dagster.assets.databricks.databricks import databricks_workflow
+from dagster.jobs import formula_one_pipeline_asset_job
+from dagster.schedules import formula_one_pipeline_schedule
 
 
 defs = Definitions(
--- dagster/schedules/__init__.py
+++ dagster/schedules/__init__.py
@@ -1,5 +1,5 @@
 from dagster import ScheduleDefinition
-from analytics.jobs import formula_one_pipeline_asset_job
+from dagster.jobs import formula_one_pipeline_asset_job
 
 formula_one_pipeline_schedule = ScheduleDefinition(
     job=formula_one_pipeline_asset_job, cron_schedule="*/5 * * * *"
