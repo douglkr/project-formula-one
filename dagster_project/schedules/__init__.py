076bad5c848a6376a09b69bb209b1ea8e143fcd4
Doug <152557999+douglkr@users.noreply.github.com>
2024-04-06 13:21:53 -0300

    refactor code

--- dagster_project/schedules/__init__.py
+++ dagster_project/schedules/__init__.py
@@ -1,5 +1,5 @@
 from dagster import ScheduleDefinition
-from dagster.jobs import formula_one_pipeline_asset_job
+from dagster_project.jobs import formula_one_pipeline_asset_job
 
 formula_one_pipeline_schedule = ScheduleDefinition(
     job=formula_one_pipeline_asset_job, cron_schedule="*/5 * * * *"
