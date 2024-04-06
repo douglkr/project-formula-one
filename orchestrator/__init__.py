a87d7b98746343c5f1ef9874a5038aa88bd60a48
Doug <152557999+douglkr@users.noreply.github.com>
2024-04-06 15:23:03 -0300

    refactor

--- orchestrator/__init__.py
+++ orchestrator/__init__.py
@@ -1,10 +1,10 @@
 from dagster import Definitions
 
-from dagster_project.resources import databricks_client_instance
-from dagster_project.assets.airbyte.airbyte import airbyte_ingestion
-from dagster_project.assets.databricks.databricks import databricks_workflow
-from dagster_project.jobs import formula_one_pipeline_asset_job
-from dagster_project.schedules import formula_one_pipeline_schedule
+from orchestrator.resources import databricks_client_instance
+from orchestrator.assets.airbyte.airbyte import airbyte_ingestion
+from orchestrator.assets.databricks.databricks import databricks_workflow
+from orchestrator.jobs import formula_one_pipeline_asset_job
+from orchestrator.schedules import formula_one_pipeline_schedule
 
 
 defs = Definitions(
